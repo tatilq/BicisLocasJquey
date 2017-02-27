102783e8135074e5c328a9b260dcdfc22739877a
Tatiana Limache Quispe <Tati34che@gmail.com>
2017-02-27 09:44:38 -0500

    Añadi sweet alert

--- js/main.js
+++ js/main.js
@@ -1,37 +1,39 @@
+//*----------------------Funcion que se ejecuta apenas se cargue el documento------------------------//
+$(document).ready(init);
 //*----------------------Funcion que asigna eventos y se ejecuta cuando cargue el documento------------------------//
-$(document).ready(function()
+function init()
 {
 	$("#name").keyup(validaName); 
 	$("#lastname").keyup(validaLastName); 
 	$("#email").keyup(validaEmail); 
 	$("#password").keyup(validaPass); 
 	$("#select_bici").click(validaBici); 
-});
+}
 //------------------------------------Funcion principal que valida el formulario-------------------------------------//
 function validateForm()
 {
   var salidaForm=$('#salidaForm');
-  validaName();
-  validaLastName();
-  validaEmail();
-  validaPass();
-  validaBici();
+  validaName(), validaLastName(), validaEmail(), validaPass(), validaBici();
 
   if(validaName() && validaLastName() && validaEmail() && validaPass() && validaBici()){
-  	 salidaForm.html('<p style="color:green;" class="animated rubberBand">Formulario Enviado Correctamente ✔</p>');  
+  	swal({
+      title: "¡Formulario correctamente enviado!",
+      imageUrl: "img/goods.png"
+    });
   }
   else{
-     salidaForm.html('<p style="color:red;" class="animated rubberBand">Llena todos los campos correctamente</p>');
+    swal({
+      title: "¡Formulario Incompleto!",
+      text:"LLenar todos los campos correctamente",
+      imageUrl: "img/bads.png"
+    });
   }
 }
-
 //-----------------------------------Funcion que crea nodos de tipo SPAN------------------------------------------//
 function crearSpan(container,mensaje)
 {    	
-    if(container.find('span').length==0)
-    {
-    	container.append("<span class='animated fadeInLeft'> "+mensaje+"</span>");
-    }
+  if(container.find('span').length==0)
+    container.append("<span class='animated fadeInLeft'> "+mensaje+"</span>");
 }
 //------------------------------------FUNCION QUE CONVIERTE LA PRIMERA LETRA EN MAYUSCULA-------------------------//
 function primMayuscula(id){
@@ -65,11 +67,10 @@ function validaName()
  		$("#contenedorName span").remove();
  		isValid=true;
  	}
-   	else{
-   		crearSpan(contenedorName,"Debes Ingresar un nombre valido (solo letras).");
-   		isValid=false;
-  	}
-  	return isValid;
+  else
+   	 crearSpan(contenedorName,"Debes Ingresar un nombre valido (solo letras).");
+  	
+  return isValid;
 }
 //-------------------------------------------VALIDA APELLIDO SEGUN FORMATO VALIDO----------------------------------//
 function validaLastName() 
@@ -78,37 +79,29 @@ function validaLastName()
 	var lastname=$("#lastname");
 	var isValid=false;
 
- 	if(lastname.val().length > 0 && lastname.val().match(/^[a-zA-Z\s]*$/))
- 	{
+ 	if(lastname.val().length > 0 && lastname.val().match(/^[a-zA-Z\s]*$/)){
  		lastname.val(primMayuscula(lastname.val()));
  		$("#contenedorLastname span" ).remove();
  		isValid=true;
  	}
-   	else
-   	{
-		crearSpan(contenedorLastname,"Debes Ingresar un apellido valido (solo letras).");
-		isValid=false;
-  	}
-
-  	return isValid;
+  else
+		 crearSpan(contenedorLastname,"Debes Ingresar un apellido valido (solo letras).");
+  	
+  return isValid;
 }
 //---------------------------------------------VALIDA EMAIL SEGUN FORMATO VALIDO-----------------------------------//
 function validaEmail() 
 { 
-  	var contenedorEmail=$("#contenedorEmail");
-  	var email=$("#email").val();
-  	var isValid=false;
+	var contenedorEmail=$("#contenedorEmail");
+	var email=$("#email").val();
+	var isValid=false;
 
- 	if(email.length > 0 && email.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/))
- 	{ 
-	    $("#contenedorEmail span" ).remove();
-	    isValid=true;
+ 	if(email.length > 0 && email.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){ 
+     $("#contenedorEmail span" ).remove();
+     isValid=true;
  	}
  	else
- 	{
-	    crearSpan(contenedorEmail,"Debes Ingresar un email valido");
-	    isValid=false;
-  	}
+	  crearSpan(contenedorEmail,"Debes Ingresar un email valido");
 
   return isValid;
 }
@@ -116,39 +109,31 @@ function validaEmail()
 function validaPass()
 {
 	var contenedorPassword=$("#contenedorPassword");
-    var password=$("#password").val();
-  	var isValid=false;
+  var password=$("#password").val();
+	var isValid=false;
 
-  	if(!password.match(/.{6,}/) || password=="123456" || password.toLowerCase()=="password" || password=="098754")
-  	{
-   	 	crearSpan(contenedorPassword,"Debes Ingresar una contraseña valida");
-    	isValid=false;
-  	}
-  	else
-  	{
-    	$("#contenedorPassword span" ).remove();
-    	isValid=true;
-   
-  	}
+	if(!password.match(/.{6,}/) || password=="123456" || password.toLowerCase()=="password" || password=="098754")
+ 	 	crearSpan(contenedorPassword,"Debes Ingresar una contraseña valida");
+	else{
+  	$("#contenedorPassword span" ).remove();
+  	isValid=true;
+	}
   return isValid;
 }
 //-----------------------------------------VALIDA QUE SELECCIONES UN TIPO DE BICICLETA------------------------------//
 function validaBici()
 {
 	var contenedorBici=$("#contenedorBici");
-  	var tipo=$("#select_bici").val();
-  	var isValid=false;
+	var tipo=$("#select_bici").val();
+	var isValid=false;
 
-  	if(tipo!=0)
-  	{
-    	$("#contenedorBici span" ).remove();
-    	isValid=true;
-  	} 
-  	else
-  	{   
-    	crearSpan(contenedorBici,"Debes seleccionar un tipo de  Bicicleta.");
-    	isValid=false;
-  	}   
+	if(tipo!=0){
+  	$("#contenedorBici span" ).remove();
+  	isValid=true;
+	} 
+	else
+  	crearSpan(contenedorBici,"Debes seleccionar un tipo de  Bicicleta.");
+	
   return isValid;
 } 
 //------------------------------------------------------------FIN--------------------------------------------------//
